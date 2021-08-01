7593ecd55cb0eb28dc061aef55a2eaa32466e340
aguibou32 <aguibou@gmail.com>
2021-08-01 11:25:32 +0200

    home page broken into small components and styling done

--- src/components/menu-items/MenuItem.jsx
+++ src/components/menu-items/MenuItem.jsx
@@ -1,20 +1,21 @@
-import React from 'react'
-import './MenuItem.scss';
+import React from "react";
+import "./MenuItem.scss";
 
-function MenuItem({title, imageUrl, size}) {
-    return (
-        <div
-          style={{
-            backgroundImage:`url(${imageUrl})`
-          }}
-
-         className={`${size} menu-item `}>
-          <div className="content">
-            <h1 className="title">{title.toUpperCase()}</h1>
-            <span className="subtitle">Shop Now</span>
-          </div>
-        </div>
-    )
+function MenuItem({ title, imageUrl, size }) {
+  return (
+    <div className={`${size} menu-item `}>
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className="content">
+        <h1 className="title">{title.toUpperCase()}</h1>
+        <span className="subtitle">Shop Now</span>
+      </div>
+    </div>
+  );
 }
 
-export default MenuItem
+export default MenuItem;
