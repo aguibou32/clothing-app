61d1183dbbe473d874c4bdca454bec87cbf9dfa4
aguibou32 <aguibou@gmail.com>
2021-08-02 20:03:54 +0200

    more changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import HomePage from './components/pages/homepage/HomePage';
+import Products from './components/pages/Products';
+
+import {Route, Switch} from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/products" component={Products} />
+      </Switch>
     </div>
   );
 }
